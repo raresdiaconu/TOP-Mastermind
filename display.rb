5207f48185fd1a97010501453314be876ef8287b
Rares Diaconu <98269458+raresdiaconu@users.noreply.github.com>
2022-06-10 14:53:26 +0300

    add instructions and feedback messages

--- display.rb
+++ display.rb
@@ -2,11 +2,42 @@
 
 require 'rainbow/refinement'
 using Rainbow
+require 'pry-byebug'
 
 # Contains the messages displayed to the player.
 module Display
+  def display_instructions
+    puts <<~HEREDOC.strip
+      Welcome to Mastermind!
+
+      The computer will create a code of 4 colors.
+      You will have 12 rounds to guess that code by inputting a combination of 4 of the colors below.
+      #{display_all_options}
+
+      After each round you will be getting #{'hints'.underline}:
+      #{solid_peg} - means that you have placed the right color in the right position.
+      #{empty_peg} - means that you have placed the right color in the wrong position.
+
+      Good luck!
+      ----------
+    HEREDOC
+  end
+
+  def display_all_options
+    options = []
+    6.times do |i|
+      options << colors((i + 1).to_s)
+    end
+    options.join('')
+  end
+
   def display_prompt_guess
-    'Input your 4 digits guess. Numbers between 1 and 6 only.'
+    rounds = @rounds_played.to_s
+    <<~HEREDOC
+
+      #{"Round #{rounds}".underline}
+      Input your combination of choice. Use digits between 1 and 6 only.
+    HEREDOC
   end
 
   def display_player_guess(guess)
@@ -18,22 +49,39 @@ module Display
   end
 
   def display_game_lost
-    'Try again. Game lost.'
+    "\nShucks! You don't seem to have cracked the code. The code was:"
   end
 
   def display_another_game
-    'Would you like to play again?'
+    "\nWould you like to play again? (Y/N)"
   end
 
   def display_invalid_input
     'Your input is invalid.'
   end
 
-  def solid_pin
+  def solid_peg
     '●'.red
   end
 
-  def empty_pin
+  def empty_peg
     '○'.red
+  end
+
+  def display_pegs
+    puts "HINTS: #{@pegs.shuffle.join(' ')}"
+    @pegs = []
+  end
+
+  def colors(color)
+    colors = {
+      '1' => '   1   '.white.bright.bg(:red),
+      '2' => '   2   '.white.bright.bg(:yellow),
+      '3' => '   3   '.white.bright.bg(:magenta),
+      '4' => '   4   '.white.bright.bg(:cyan),
+      '5' => '   5   '.black.bright.bg(:silver),
+      '6' => '   6   '.white.bright.bg(:black)
+    }
+    colors[color]
   end
 end
