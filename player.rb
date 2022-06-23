d96df258767b6eac0e63921cc6012f68f0df22ac
Rares Diaconu <98269458+raresdiaconu@users.noreply.github.com>
2022-06-23 12:26:14 +0300

    create user code input and return it to the game class

--- player.rb
+++ player.rb
@@ -1,5 +1,22 @@
 # frozen_string_literal: true
 
+require_relative 'Display'
+require_relative 'Game'
+
 # Gets the player's input: guesses and input.
 class Player
+  include Display
+
+  def input_code
+    puts display_input_code
+    player_code = gets.chomp
+    return invalid_code_input unless player_code.chars.uniq.length == 4 && player_code.match(/[1-6]/)
+
+    player_code.chars.map!(&:to_i)
+  end
+
+  def invalid_code_input
+    puts display_invalid_input
+    input_code
+  end
 end
