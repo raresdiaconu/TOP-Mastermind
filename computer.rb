66a0f4acf5dc2b824baad557d642cbff2b418bdd
Rares Diaconu <98269458+raresdiaconu@users.noreply.github.com>
2022-06-25 10:26:09 +0300

    Make computer algorithm work, win game in max 5 rounds

--- computer.rb
+++ computer.rb
@@ -1,34 +1,48 @@
 # frozen_string_literal: true
 
+require_relative 'Display'
 require_relative 'Game'
+require 'pry-byebug'
 
 # Generates a random code when player is guessing and contains the computer guessing logic.
 class Computer
-  attr_accessor :all_permutations, :all_candidates
+  include Display
+  attr_accessor :all_permutations, :all_candidates, :new_candidates, :initial_guess, :guess, :current_pegs
 
   def initialize
-    @range = %w[1 2 3 4 5 6]
+    @range = [1, 2, 3, 4, 5, 6]
     @all_permutations = create_all_permutations(@range)
-    @all_candidates = @all_permutations
-    @initial_guess = %w[1 1 2 2]
+    # @all_permutations = [[1, 1, 2, 2], [1, 2, 3, 4], [1, 2, 1, 1]]
+    @all_candidates = @all_permutations.clone
+    @new_candidates = @all_candidates.clone
+    @initial_guess = [1, 1, 2, 2]
+    @guess = []
+    @current_pegs = []
   end
 
   def create_all_permutations(range)
     range.repeated_permutation(4).to_a
   end
 
-  def computer_guess
-    return @guess = @initial_guess if @rounds_played == 1
-
-    @guess = @all_candidates.sample(1)
+  def make_guess(rounds_played)
+    # return @guess = initial_guess if rounds_played == 1
+    if rounds_played == 1
+      @guess = initial_guess
+      apply_color(@guess)
+      return @guess
+    end
+    @guess = @all_candidates.sample(1).flatten
+    apply_color(@guess)
+    @guess
   end
 
-  # def make_move
-
-  # end
-
-  def compare_guess(_hints, _guess)
-    @temp_pegs = @pegs
+  def sort_new_candidates(pegs)
+    @all_candidates.each do |candidate|
+      computer_check_right_pos(candidate, @guess)
+      @new_candidates.delete(candidate) unless current_pegs == pegs
+      current_pegs.clear
+    end
+    @all_candidates = @new_candidates.clone
 
     # make the initial guess and get the first set of pegs.
     # assign the value of @pegs to another instance variable, called @temp_pegs
@@ -38,10 +52,32 @@ class Computer
     # after getting the number and type of pegs (another array) compare it to the @temp_pegs (initial guess)
     # if the number and type of pegs are different (arrays are not equal),
     # delete the currently assessed code from the @all candidates array
-    # 
+    #
   end
 
-  def self.generate_code
+  def computer_check_right_pos(candidate, current_guess)
+    temp_code = []
+    temp_guess = []
+    candidate.each_with_index do |digit, idx|
+      if digit == current_guess[idx]
+        current_pegs << solid_peg
+        temp_code << nil
+        temp_guess << nil
+      else
+        temp_code << digit
+        temp_guess << current_guess[idx]
+      end
+    end
+    computer_check_any_pos(temp_guess, temp_code)
+  end
+
+  def computer_check_any_pos(current_guess, temp_code)
+    current_guess.uniq.each do |digit|
+      current_pegs << empty_peg if temp_code.include?(digit) && !digit.nil?
+    end
+  end
+
+  def generate_code
     (1..6).to_a.sample(4)
   end
 end
